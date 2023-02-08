53ad8fefe110040b9456e3ed33ca6539ac8e266c
michaelaguilar89 <mchlaguilar051@hotmail.es>
2023-02-07 21:02:30 -0600

    use dependecy injectio i reference to default connection string on appsettings.json

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using WebApi_Control_Production.Connection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext <ApplicationDbContext>(options =>{//we use dependecy injection
+																//i reference to defaultConnection string
+
+	options.UseSqlServer(
+		builder.Configuration.GetConnectionString("defaultConnection"));
+});
 
 var app = builder.Build();
 
