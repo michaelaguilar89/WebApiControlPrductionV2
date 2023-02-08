d6a82b5f185e7ce04d2174c4d19fbb0aa573022f
michaelaguilar89 <mchlaguilar051@hotmail.es>
2023-02-07 20:52:46 -0600

    update myResponse Class

--- Models/myResponse.cs
+++ Models/myResponse.cs
@@ -4,10 +4,10 @@
 	{
 		public bool IsSucces { get; set; } = true;
 
-		public Object Result { get; set; } 
+		public Object? Result { get; set; } 
 
-		public string DisplayMessage { get; set; }
+		public string? DisplayMessage { get; set; }
 
-		public List<string> ErrorMessages { get; set; }
+		public List<string>? ErrorMessages { get; set; }
 	}
 }
